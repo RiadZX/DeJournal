3877aacb39014eb2aeec6985b20a266e988f676b
riad <riadzaid100@gmail.com>
2025-08-22 21:21:06 +0200

    reset project

--- deBlog/next.config.ts
+++ deBlog/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  transpilePackages: [
-    '@solana/wallet-adapter-react',
-    '@solana/web3.js',
-    '@project-serum/anchor',
-  ],
+  /* config options here */
 }
 
 export default nextConfig
